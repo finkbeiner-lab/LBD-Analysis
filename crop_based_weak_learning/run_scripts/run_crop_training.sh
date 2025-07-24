cabbccc0c311f60e72581f084050e2948222ea2d
Monika Ahirwar <117786920+monika-ahirwar@users.noreply.github.com>
2025-07-24 10:59:18 -0700

    Add files via upload

---
+++ crop_based_weak_learning/run_scripts/run_crop_training.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=clf      ## Name of the job
+#SBATCH --output=clf3.out    ## Output file
+#SBATCH --time=07:59:00           ## Job Duration
+#SBATCH --ntasks=1             ## Number of tasks (analyses) to run
+#SBATCH --cpus-per-task=8     ## The number of threads the code will use
+#SBATCH --mem=100G     ## Real memory(MB) per CPU required by the job.
+#SBATCH --gres=gpu:1
+#SBATCH --partition=kif-dev
+
+## Load the python interpreters
+
+source /gladstone/finkbeiner/home/mahirwar/miniforge3/etc/profile.d/conda.sh
+#module load cuda/12.4
+conda activate gigapath2
+module load cuda/12.4
+
+cd /gladstone/finkbeiner/steve/work/data/npsad_data/monika/Antibodies_detection/codes/crop_based
+python3 train_crop_classifer.py 
